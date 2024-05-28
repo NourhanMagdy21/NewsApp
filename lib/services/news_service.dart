6f1268dbd1ddf22cf193569b080441e949ec997c
NourhanMagdy21 <150091752+NourhanMagdy21@users.noreply.github.com>
2024-05-28 21:07:33 +0300

    final

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -6,19 +6,20 @@ class NewsService {
 
   NewsService(this.dio);
 
-  Future<List<ArticleModel>> getGeneralNews() async {
-    Response response = await dio.get(
-        'https://newsapi.org/v2/top-headlines?country=us&apiKey=f882c2b8a234430f88150300d5c3723d&category=general');
-    Map<String, dynamic> jsonData = response.data;
-    List<dynamic> articles = jsonData['articles'];
-    List<ArticleModel> articlesList = [];
-    for (var article in articles) {
-      ArticleModel articleModel = ArticleModel(
-          image: article['urlToImage'],
-          title: article['title'],
-          subTitle: article['description']);
-      articlesList.add(articleModel);
+  Future<List<ArticleModel>> getTopHeadlines({required String category}) async {
+    try {
+      Response response = await dio.get(
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=f882c2b8a234430f88150300d5c3723d&category=$category');
+      Map<String, dynamic> jsonData = response.data;
+      List<dynamic> articles = jsonData['articles'];
+      List<ArticleModel> articlesList = [];
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel.fromJson(article);
+        articlesList.add(articleModel);
+      }
+      return articlesList;
+    }  catch (e) {
+     return[];
     }
-    return articlesList;
   }
 }
--- lib/widgets/news_tile_list_view.dart
+++ lib/widgets/news_tile_list_view.dart
@@ -1,38 +1,17 @@
-import 'package:dio/dio.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:news_application/services/news_service.dart';
 import '../models/article_model.dart';
 import 'news_tile.dart';
 
-class NewsTileListView extends StatefulWidget {
-  const NewsTileListView({super.key});
+class NewsTileListView extends StatelessWidget {
+  final List<ArticleModel> articles;
 
-  @override
-  State<NewsTileListView> createState() => _NewsTileListViewState();
-}
-
-class _NewsTileListViewState extends State<NewsTileListView> {
-  List<ArticleModel> articles = [];
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    getGeneralNews();
-  }
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsService(Dio()).getGeneralNews();
-    isLoading = false;
-    setState(() {});
-  }
+const NewsTileListView({super.key, required this.articles});
 
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()))
-        : SliverList(
+    return SliverList(
             delegate: SliverChildBuilderDelegate(childCount: articles.length,
                 (context, index) {
             return Padding(
